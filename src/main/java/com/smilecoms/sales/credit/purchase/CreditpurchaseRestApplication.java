595fb21ef681b2492653162f4590a64512d08ef4
tebogo.molokomme <tebogo.molokomme@investec.co.za>
2021-06-11 21:10:32 +0200

    initial commit

---
+++ src/main/java/com/smilecoms/sales/credit/purchase/CreditpurchaseRestApplication.java
@@ -0,0 +1,11 @@
+package com.smilecoms.sales.credit.purchase;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+/**
+ *
+ */
+@ApplicationPath("/data")
+public class CreditpurchaseRestApplication extends Application {
+}
